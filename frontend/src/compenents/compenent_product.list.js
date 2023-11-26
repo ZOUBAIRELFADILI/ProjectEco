e12cd623ab104755506beca8bc322e16372d5e61
zoubairelfadili <elfadili.zoubair@gmail.com>
2023-11-26 11:31:28 +0100

    Push

---
+++ frontend/src/compenents/compenent_product.list.js
@@ -0,0 +1,37 @@
+import {useEffect,useState} from "react";
+import {deleteProductByID,getAllProducts,getAllproducts} from "../services/product.services"
+import { Link } from "react-router-dom";
+
+export  function ProductList(){
+    const [products,setProducts]=useState([]);
+    useEffect(()=>{
+        fetchProducts();
+    },[])
+    async function fetchProducts(){
+        const res=await getAllProducts()
+        setProducts(res.data);
+    }
+    async function deleteProduct(id){
+        const res=await deleteProductByID(id)
+        fetchProducts();
+    }
+    return(
+      <>
+        <table className='etudiantTable'>
+      <thead>
+      <tr>
+        <th>Nom</th>
+        <th>Age</th>
+      </tr>
+      </thead>
+      <tbody>
+      {products.map((elem,index)=><tr key={index}>
+        <td>{elem.name}</td>
+        <td>{elem.price}</td>
+        <td><button onClick={() => deleteProduct(elem._id)}>Delete</button></td>
+        </tr>)}
+      </tbody>
+  </table>
+  <Link to={"/products/new"}>Nouveau produit</Link>
+  </>);
+}
